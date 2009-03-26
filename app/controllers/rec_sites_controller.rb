7da4d0f194d68a1247baa3aa2911946b86d8c426
schoe <schoe@schoe-desktop.(none)>
2009-03-26 11:22:12 -0400

    Automatic rec_sites_helper.rb file getting added when controller is created.

---
+++ app/controllers/rec_sites_controller.rb
@@ -0,0 +1,26 @@
+require 'QueryHelper/StructureQuery'
+require 'rexml/document'
+
+class RecSitesController < ApplicationController
+include REXML
+
+     def keywords
+          @header= '<recommended_site_search>'
+          @footer= '</recommended_site_search>'
+          keywords = params[:id]
+	     string=''
+	     @books = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getKeywordsQuery(keywords))
+	     @books.each do |c|
+		  string = string+<<EOF      
+		       <recommended_site recType="keywords">
+		       <title>#{c.title}</title>
+		       <url>#{c.url}</url>
+		       <description>#{c.description}</description>
+		       </recommended_site>
+EOF
+             end
+
+          @doc = Document.new @header+string+@footer
+          render :xml => @doc
+      end
+end
