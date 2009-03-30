82ba2759a70d5618eef259341abd7430ac1ce358
schoe <schoe@schoe-desktop.(none)>
2009-03-30 16:33:09 -0400

    Added some examples of the web service GET calls in comments.  Cleaned up the code to pull out the repeative use of similar XML format to RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType) for reuse.

--- app/controllers/rec_sites_controller.rb
+++ app/controllers/rec_sites_controller.rb
@@ -1,29 +1,45 @@
 require 'QueryHelper/StructureQuery'
+require 'XmlHelper/StructureXml'
 require 'rexml/document'
   
 class RecSitesController < ApplicationController
 include REXML
 
-     def keywords
-          @header= '<recommended_site_search>'
-          @footer= '</recommended_site_search>'
-          keywords = params[:id]
-	     string=''
-	     @queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getKeywordsQuery(keywords))
-	     @queryResults.each do |c|
-		  string = string+<<EOF      
-		       <recommended_site recType="keywords">
-		       <title>#{c.title}</title>
-		       <url>#{c.url}</url>
-		       <description>#{c.description}</description>
-		       </recommended_site>
-EOF
-             end
+	def initialize
+          #@header= '<recommended_site_search>'
+          #@footer= '</recommended_site_search>'
+	end
 
-          @doc = Document.new @header+string+@footer
+	#http://localhost:3000/rec_sites/keywords/bankruptcy
+     def keywords
+          @recType='keywords'
+          keywords = params[:id]
+	     queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getKeywordsQuery(keywords))
+          @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
           render :xml => @doc
       end
 
-	def features
-	end
+	#this "show" def might not needed.  Revisit again. 3/30/09 - songchoe. 
+     #http://localhost:3000/rec_sites/94117
+	def show
+          @recType='features'
+		zipcode=params[:id]
+          string=''
+     	queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getFeaturedQueryByZipcode(zipcode))
+	     @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
+          
+          render :xml => @doc
+     end
+
+	# http://localhost:3000/rec_sites/20121/taxes
+     # http://localhost:3000/rec_sites/ebay
+     def features
+		zipcode=params[:id]
+		keywords=params[:name]
+          @recType='features'
+          string=''
+     	queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getFeaturedQueryByZipKeywords(keywords,zipcode))
+	     @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
+          render :xml => @doc
+     end
 end
