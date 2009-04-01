f1e02c7ddaf12ef315d1b938c2902357ca9edeb4
Song Choe <songchoe@song-choes-macbook-pro.local>
2009-04-01 10:42:15 -0400

    Added NotFound xml to show something is not found. <notFound>Not Found</notFound>

--- app/controllers/XmlHelper/StructureXml.rb
+++ app/controllers/XmlHelper/StructureXml.rb
@@ -20,6 +20,17 @@ EOF
 		doc = Document.new string
 		return doc	
 	end
+	
+	def RecommendedSiteXML.getRecommendedSiteNotFoundXML(recType)
+	string=''
+	 string = string+<<EOF      
+				       <recommended_site_search>
+					  <recommended_site recType="#{recType}">
+					  <notFound>Not Found</notFound>
+					  </recommended_site>
+				       </recommended_site_search>
+EOF
+	end
 end
 
 
--- app/controllers/rec_sites_controller.rb
+++ app/controllers/rec_sites_controller.rb
@@ -10,16 +10,20 @@ include REXML
           #@footer= '</recommended_site_search>'
 	end
 
-	#http://localhost:3000/rec_sites/keywords/bankruptcy
+	 #http://localhost:3000/rec_sites/keywords/bankruptcy
      def keywords
          @recType='keywords'
           keywords = params[:id]
 	     queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getKeywordsQuery(keywords))
-          @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
+          if queryResults.blank?
+           @doc = RecommendedSiteXML.getRecommendedSiteNotFoundXML(@recType)
+          else
+           @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
+          end
           render :xml => @doc
       end
 
-	#this "show" def might not needed.  Revisit again. 3/30/09 - songchoe. 
+	 #this "show" def might not needed.  Revisit again. 3/30/09 - songchoe. 
      #http://localhost:3000/rec_sites/94117
 	def show
         @recType='features'
@@ -30,14 +34,19 @@ include REXML
         render :xml => @doc
      end
 
-	# http://localhost:3000/rec_sites/20121/taxes
+	 # http://localhost:3000/rec_sites/20121/taxes
      # http://localhost:3000/rec_sites/ebay
      def features
 		zipcode=params[:id]
 		keywords=params[:name]
         @recType='features'
      	queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getFeaturedQueryByZipKeywords(keywords,zipcode))
-	    @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
+	    if queryResults.blank?
+           @doc = RecommendedSiteXML.getRecommendedSiteNotFoundXML(@recType)
+        else
+           @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
+        end
+	   
         render :xml => @doc
      end
 end
