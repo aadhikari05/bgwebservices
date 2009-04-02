16da1820ee5f962c771fe32cee69d8cb4cf1ab4f
schoe <schoe@schoe-desktop.(none)>
2009-04-02 15:40:30 -0400

    Modified the xml structure so that it has <Plural><singular> format, with id.

--- app/controllers/XmlHelper/StructureXml.rb
+++ app/controllers/XmlHelper/StructureXml.rb
@@ -4,19 +4,21 @@ module RecommendedSiteXML
 include REXML
 	
 	def RecommendedSiteXML.getRecommendedSiteXML(queryResults, recType)
-		string=''
+		string='<recommended_site_searches>'
           
 		queryResults.each do |c|
 				  string = string+<<EOF      
-				       <recommended_site_search>
-					  <recommended_site recType="#{recType}">
+					  <recommended_site_search id="#{c.id}">
+				       <recommended_site recType="#{recType}">
 					  <title>#{c.title}</title>
 					  <url>#{c.url}</url>
 					  <description>#{c.description}</description>
 					  </recommended_site>
-				       </recommended_site_search>
+					  </recommended_site_search>
+				       
 EOF
 		end
+          string = string+'</recommended_site_searches>'
 		doc = Document.new string
 		return doc	
 	end
