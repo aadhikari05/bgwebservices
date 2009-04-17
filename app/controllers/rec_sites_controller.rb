00200f94737b817e16d7fa7b3af07c16e2563a02
Song Choe <songchoe@song-choes-macbook-pro.local>
2009-04-17 15:10:00 -0400

    taking xml structure out from the controller to the views. Using the builder to generate xml

--- app/controllers/rec_sites_controller.rb
+++ app/controllers/rec_sites_controller.rb
@@ -1,57 +1,37 @@
 require 'QueryHelper/StructureQuery'
-require 'XmlHelper/StructureXml'
-require 'rexml/document'
-  
+require 'builder'
+
 class RecSitesController < ApplicationController
-include REXML
-
-	def initialize
-          #@header= '<recommended_site_search>'
-          #@footer= '</recommended_site_search>'
-	end
-
-	 #http://localhost:3000/rec_sites/keywords/bankruptcy
-	 #http://localhost:3000/rec_sites/keywords/ebay
-     def keywords
-         @recType='keywords'
-          keywords = params[:keyword]
-	     queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getKeywordsQuery(keywords))
-          if queryResults.blank?
-           @doc = RecommendedSiteXML.getRecommendedSiteNotFoundXML(@recType)
-          else
-           @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
-          end
-          render :xml => @doc
-      end
-
-	 #this "show" def might not needed.  Revisit again. 3/30/09 - songchoe. 
-     #http://localhost:3000/rec_sites/94117
-	def show
-        @recType='features'
-		zipcode=params[:keyword]
-     	queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getFeaturedQueryByZipcode(zipcode))
-	    if queryResults.blank?
-           @doc = RecommendedSiteXML.getRecommendedSiteNotFoundXML(@recType)
-        else
-           @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
-        end  
-
-        render :xml => @doc
-     end
-
-	 # http://localhost:3000/rec_sites/20121/taxes
-
-     def features
-		zipcode=params[:keyword]
-		keywords=params[:name]
-        @recType='features'
-     	queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getFeaturedQueryByZipKeywords(keywords,zipcode))
-	    if queryResults.blank?
-           @doc = RecommendedSiteXML.getRecommendedSiteNotFoundXML(@recType)
-        else
-           @doc = RecommendedSiteXML.getRecommendedSiteXML(queryResults, @recType)
-        end
-	   
-        render :xml => @doc
-     end
+  
+  def initialize
+    @xml = Builder::XmlMarkup.new(:indent => 2)
+  end
+  
+  #http://localhost:3000/rec_sites/keywords/bankruptcy
+  #http://localhost:3000/rec_sites/keywords/ebay
+  def keywords
+    @recType='keywords'
+    keywords = params[:keyword]
+    @queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getKeywordsQuery(keywords)) 
+    render :template => 'recommendedSiteResult.builder'
+  end
+  
+  #this "show" def might not needed.  Revisit again. 3/30/09 - songchoe. 
+  #http://localhost:3000/rec_sites/94117
+  def show
+    @recType='features'
+    zipcode=params[:keyword]
+    @queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getFeaturedQueryByZipcode(zipcode))
+    render :template => 'recommendedSiteResult.builder'
+  end
+  
+  # http://localhost:3000/rec_sites/20121/taxes
+  
+  def features
+    zipcode=params[:keyword]
+    keywords=params[:name]
+    @recType='features'
+    @queryResults = RecommendedSiteCategory.find_by_sql(RecommendedSiteQuery.getFeaturedQueryByZipKeywords(keywords,zipcode))
+    render :template => 'recommendedSiteResult.builder'    
+  end
 end
