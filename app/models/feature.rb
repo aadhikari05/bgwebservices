2e4ba748bb84d78c4244674cbe577d4f513240b2
Amitabh Adhikari <aadhikari@content-studios.com>
2009-05-09 15:14:33 -0400

    Working on permitme_by_zip. Writing queries to get this function to the same starting point as permit_me_by_state_and_feature.

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,4 +1,8 @@
 class Feature < ActiveRecord::Base
   has_and_belongs_to_many :feature_recommended_sites, :join_table => "feature_recommended_site", :foreign_key => "feature_id", :association_foreign_key => "feature_recommended_site_id"
+  has_many :zipcodes
+  has_many :sites
+  has_many :alternate_names
+  has_many :permitme_sites
 
 end
