5238763b22016a0704fb0cca48bd183994bc40b4
Amitabh Adhikari <aadhikari@content-studios.com>
2009-05-27 16:10:13 -0400

    Deleting another unneeded method.

--- app/helpers/grant_loan_helper.rb
+++ app/helpers/grant_loan_helper.rb
@@ -1,17 +1,12 @@
 module GrantLoanHelper
-
-    #This method assumes a many-to-many relationship between grant_loan and state.
-    #In the production grant_loan_state table there are only two entries, though.
-    #Keeping this method in case there is a switch over to many-to-many later on.
-    def GrantLoanHelper.get_grant_loans (state_alpha)
-        GrantLoan.find(:all, :select => "agency, title, description, url", :joins => ("LEFT OUTER JOIN `grant_loan_state` ON `grant_loan_state`.grant_loan_id = `grant_loans`.id LEFT OUTER JOIN `states` ON `states`.id = `grant_loan_state`.state_id LEFT OUTER JOIN `grant_loans_industry` ON `grant_loans_industry`.grant_loans_id = `grant_loans`.id LEFT OUTER JOIN `industries` ON `industries`.id = `grant_loans_industry`.industry_id"), :conditions => (["states.alpha = ?", state_alpha]))
-    end
     
     def GrantLoanHelper.get_grants_and_loans (state_alpha, business_type, industry, business_task)
         state_id = GrantLoanHelper.getStateIDFromStateAlpha(state_alpha)
+        state_name = GrantLoanHelper.getStateIDFromStateAlpha(state_alpha)
         
         #Get results individually based on the parameters and then add to the results
-        type_array = ["general_purpose","development","exporting","contractor","green","military","minority","woman","disabled","rural","disaster"]
+#        type_array = ["general_purpose","development","exporting","contractor","green","military","minority","woman","disabled","rural","disaster"]
+        type_array = business_task.split("-")
         result_array = Array.new
         
         type_array.each do |type_name|
@@ -38,8 +33,8 @@ module GrantLoanHelper
         State.find(:first, :select => "id", :conditions => ["alpha = ?",state_alpha])
     end
 
-    def GrantLoanHelper.getStateAlphaFromStateID(state_id)
-        State.find(:first, :select => "alpha", :conditions => ["id = ?",state_id])
+    def GrantLoanHelper.getStateNameFromStateAlpha(state_alpha)
+        State.find(:first, :select => "name", :conditions => ["alpha = ?",state_alpha])
     end
 
     
