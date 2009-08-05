1162fbc42617f16f5ede6b0105b0333f080da03c
Amitabh Adhikari <aadhikari@content-studios.com>
2009-08-05 11:15:42 -0400

    Added site_filters to features model.

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,6 +2,7 @@ class Feature < ActiveRecord::Base
   has_and_belongs_to_many :feature_recommended_sites, :join_table => "feature_recommended_site", :foreign_key => "feature_id", :association_foreign_key => "feature_recommended_site_id"
   has_many :zipcodes
   has_many :sites
+  has_many :site_filters
   has_many :alternate_names
   has_many :permitme_sites
 
