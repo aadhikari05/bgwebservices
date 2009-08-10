0e98b07d523de35ff005064efc30f22e3b5645f3
Amitabh Adhikari <aadhikari@content-studios.com>
2009-08-10 14:50:25 -0400

    Added geodata_result.rb

---
+++ app/models/geodata_result.rb
@@ -0,0 +1,83 @@
+class GeodataResult
+  
+  attr_accessor :county_sites, :local_sites, :state_sites
+  
+  def initialize
+    @county_sites = Array.new
+    @local_sites = Array.new
+    @state_sites = Array.new
+  end
+  
+  
+  def to_xml(options = {})
+      xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
+      xml.instruct! unless options[:skip_instruct => true, :dasherize => false]
+    
+      @county_sites.sort! {|a,b| a.link_title <=> b.link_title}
+      @local_sites.sort! {|a,b| a.link_title <=> b.link_title }
+      @state_sites.sort! {|a,b| a.link_title <=> b.link_title}
+    
+      xml.result do
+          xml.county_sites do |site|
+              for current_site in 0...@county_sites.length
+                xml.site do
+                  site.link_title(@county_sites[current_site]["link_title"])
+                  site.description(@county_sites[current_site]["description"])
+                  site.url(@county_sites[current_site]["url"])
+                end
+              end
+          end
+        
+          xml.local_sites do |site|
+              for current_site in 0...@local_sites.length
+                xml.site do
+                  site.link_title(@local_sites[current_site]["link_title"])
+                  site.description(@local_sites[current_site]["description"])
+                  site.url(@local_sites[current_site]["url"])
+                end
+              end
+          end
+        
+          xml.state_sites do |site|
+              for current_site in 0...@state_sites.length
+                xml.site do
+                  site.link_title(@state_sites[current_site]["link_title"])
+                  site.description(@state_sites[current_site]["description"])
+                  site.url(@state_sites[current_site]["url"])
+                end
+              end
+          end
+      end
+  end
+  
+  def to_json(options = {})
+      result = Hash.new
+	
+    	for current_site in 0...@county_sites.length
+    		county_site_value=Array.new
+    		county_site_value.push({"title"=>@county_sites[current_site]["link_title"]})
+    		county_site_value.push({"description"=>@county_sites[current_site]["description"]})
+    		county_site_value.push({"url"=>@county_sites[current_site]["url"]})
+    		h1={"county_sites_item"+current_site.to_s =>state_site_value}
+    		result.merge!(h1) 
+    	end
+    	for current_site in 0...@local_sites.length
+    		local_site_value=Array.new
+    		local_site_value.push({"title"=>@local_sites[current_site]["link_title"]})
+    		local_site_value.push({"description"=>@local_sites[current_site]["description"]})
+    		local_site_value.push({"url"=>@local_sites[current_site]["url"]})
+    		h1={"state_site_item"+current_site.to_s =>local_site_value}
+    		result.merge!(h1) 
+    	end
+    	for current_site in 0...@state_sites.length
+    		state_site_value=Array.new
+    		state_site_value.push({"title"=>@state_sites[current_site]["link_title"]})
+    		state_site_value.push({"description"=>@state_sites[current_site]["description"]})
+    		state_site_value.push({"url"=>@state_sites[current_site]["url"]})
+    		h1={"state_site_item"+current_site.to_s =>state_site_value}
+    		result.merge!(h1) 
+    	end
+      result.to_json  
+  end
+
+end
