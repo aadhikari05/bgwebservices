d03ffb8e49e66a7f56271aff7f5bdc38b821368c
Amitabh Adhikari <aadhikari@content-studios.com>
2009-08-12 17:14:04 -0400

    Modified geodata_by_majorfeature to return county sites for major feature.

--- app/controllers/geodata_controller.rb
+++ app/controllers/geodata_controller.rb
@@ -17,11 +17,12 @@ class GeodataController < ApplicationController
       def geodata_by_major_city
           #http://localhost:3000/geodata/major_city/anchorage.xml        
           #We take the state alpha and use it to get state_id,(fips_feature_id, feature_id will be blank in this cases)
+          @majorfeature = GeodataHelper.getMajorFeatureByFeatureName(params[:feature])
           @state_and_feature = GeodataHelper.getStateIDFromStateAlpha(params[:alpha])
           @state_alpha = params[:alpha]
         
           #We pass the state_id and fips_feat_id to the function below to get the list of County Sites
-          respond_to_format(GeodataHelper.get_all_geodata_sites(@state_and_feature))
+          respond_to_format(GeodataHelper.get_all_geodata_sites(@majorfeature))
       end
 
       def geodata_by_state_and_feature
