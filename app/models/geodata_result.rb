f099bc62e70aab5e0287a87cf20f5590a0abde9e
Amitabh Adhikari <aadhikari@content-studios.com>
2009-08-12 15:04:31 -0400

    Added fips columns to geodata_result to_xml and to_json methods.

--- app/models/geodata_result.rb
+++ app/models/geodata_result.rb
@@ -24,6 +24,12 @@ class GeodataResult
                   site.link_title(@county_sites[current_site]["link_title"])
                   site.description(@county_sites[current_site]["description"])
                   site.url(@county_sites[current_site]["url"])
+                  site.fips_id(@county_sites[current_site]["fips_id"])
+                  site.fips_class(@county_sites[current_site]["fips_class"])
+                  site.fips_feat_id(@county_sites[current_site]["fips_feat_id"])
+                  site.fips_st_cd(@county_sites[current_site]["fips_st_cd"])
+                  site.fips_place_cd(@county_sites[current_site]["fips_place_cd"])
+                  site.fips_county_cd(@county_sites[current_site]["fips_county_cd"])
                 end
               end
           end
@@ -34,6 +40,12 @@ class GeodataResult
                   site.link_title(@local_sites[current_site]["link_title"])
                   site.description(@local_sites[current_site]["description"])
                   site.url(@local_sites[current_site]["url"])
+                  site.fips_id(@county_sites[current_site]["fips_id"])
+                  site.fips_class(@county_sites[current_site]["fips_class"])
+                  site.fips_feat_id(@county_sites[current_site]["fips_feat_id"])
+                  site.fips_st_cd(@county_sites[current_site]["fips_st_cd"])
+                  site.fips_place_cd(@county_sites[current_site]["fips_place_cd"])
+                  site.fips_county_cd(@county_sites[current_site]["fips_county_cd"])
                 end
               end
           end
@@ -44,6 +56,12 @@ class GeodataResult
                   site.link_title(@state_sites[current_site]["link_title"])
                   site.description(@state_sites[current_site]["description"])
                   site.url(@state_sites[current_site]["url"])
+                  site.fips_id(@county_sites[current_site]["fips_id"])
+                  site.fips_class(@county_sites[current_site]["fips_class"])
+                  site.fips_feat_id(@county_sites[current_site]["fips_feat_id"])
+                  site.fips_st_cd(@county_sites[current_site]["fips_st_cd"])
+                  site.fips_place_cd(@county_sites[current_site]["fips_place_cd"])
+                  site.fips_county_cd(@county_sites[current_site]["fips_county_cd"])
                 end
               end
           end
@@ -58,7 +76,13 @@ class GeodataResult
     		county_site_value.push({"title"=>@county_sites[current_site]["link_title"]})
     		county_site_value.push({"description"=>@county_sites[current_site]["description"]})
     		county_site_value.push({"url"=>@county_sites[current_site]["url"]})
-    		h1={"county_sites_item"+current_site.to_s =>state_site_value}
+    		county_site_value.push({"fips_id"=>@county_sites[current_site]["fips_id"]})
+    		county_site_value.push({"fips_class"=>@county_sites[current_site]["fips_class"]})
+    		county_site_value.push({"fips_feat_id"=>@county_sites[current_site]["fips_feat_id"]})
+    		county_site_value.push({"fips_st_cd"=>@county_sites[current_site]["fips_st_cd"]})
+    		county_site_value.push({"fips_place_cd"=>@county_sites[current_site]["fips_place_cd"]})
+    		county_site_value.push({"fips_county_cd"=>@county_sites[current_site]["fips_county_cd"]})
+    		h1={"county_sites_item"+current_site.to_s =>county_site_value}
     		result.merge!(h1) 
     	end
     	for current_site in 0...@local_sites.length
@@ -66,7 +90,13 @@ class GeodataResult
     		local_site_value.push({"title"=>@local_sites[current_site]["link_title"]})
     		local_site_value.push({"description"=>@local_sites[current_site]["description"]})
     		local_site_value.push({"url"=>@local_sites[current_site]["url"]})
-    		h1={"state_site_item"+current_site.to_s =>local_site_value}
+    		local_site_value.push({"fips_id"=>@county_sites[current_site]["fips_id"]})
+    		local_site_value.push({"fips_class"=>@county_sites[current_site]["fips_class"]})
+    		local_site_value.push({"fips_feat_id"=>@county_sites[current_site]["fips_feat_id"]})
+    		local_site_value.push({"fips_st_cd"=>@county_sites[current_site]["fips_st_cd"]})
+    		local_site_value.push({"fips_place_cd"=>@county_sites[current_site]["fips_place_cd"]})
+    		local_site_value.push({"fips_county_cd"=>@county_sites[current_site]["fips_county_cd"]})
+    		h1={"local_site_item"+current_site.to_s =>local_site_value}
     		result.merge!(h1) 
     	end
     	for current_site in 0...@state_sites.length
@@ -74,6 +104,12 @@ class GeodataResult
     		state_site_value.push({"title"=>@state_sites[current_site]["link_title"]})
     		state_site_value.push({"description"=>@state_sites[current_site]["description"]})
     		state_site_value.push({"url"=>@state_sites[current_site]["url"]})
+    		state_site_value.push({"fips_id"=>@county_sites[current_site]["fips_id"]})
+    		state_site_value.push({"fips_class"=>@county_sites[current_site]["fips_class"]})
+    		state_site_value.push({"fips_feat_id"=>@county_sites[current_site]["fips_feat_id"]})
+    		state_site_value.push({"fips_st_cd"=>@county_sites[current_site]["fips_st_cd"]})
+    		state_site_value.push({"fips_place_cd"=>@county_sites[current_site]["fips_place_cd"]})
+    		state_site_value.push({"fips_county_cd"=>@county_sites[current_site]["fips_county_cd"]})
     		h1={"state_site_item"+current_site.to_s =>state_site_value}
     		result.merge!(h1) 
     	end
