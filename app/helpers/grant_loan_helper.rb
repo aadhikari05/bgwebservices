b1e124f4bcf42e21270ad87434878878749119d8
Amitabh Adhikari <aadhikari@content-studios.com>
2009-12-10 11:21:36 -0500

    Fixed industry to remove business_type option

--- app/helpers/grant_loan_helper.rb
+++ app/helpers/grant_loan_helper.rb
@@ -34,7 +34,7 @@ module GrantLoanHelper
 
         if !industry.eql?("nil")
             if !GrantLoanHelper.is_industry(industry).empty?
-                result_array << GrantLoanHelper.get_industry_results (business_type, industry)
+                result_array << GrantLoanHelper.get_industry_results (industry)
             else
                 return this_result
             end
@@ -103,14 +103,16 @@ module GrantLoanHelper
         GrantLoan.find_by_sql([grant_loan_sql,state_name])
     end
 
-    def GrantLoanHelper.get_industry_results (business_type, industry)
-        industry_sql = "select state_name, business_type, title, description, url,loan_type, state_name, agency, gov_type, is_general_purpose, is_development, is_exporting, is_contractor, is_green, is_military, is_minority, is_woman, is_disabled, is_rural, is_disaster "
-        industry_sql += "from grant_loans g, grant_loans_industry gli where "
-        industry_sql += " business_type like ? and g.id= any "
-        industry_sql += "(select gli.grant_loans_id from grant_loans_industry gli, industries i "
-        industry_sql += " where gli.industry_id=i.id and i.name=?) and gli.grant_loans_id=g.id"
-        
-        GrantLoan.find_by_sql([industry_sql, '%'+business_type+'%', industry])
+    def GrantLoanHelper.get_industry_results (industry)
+      grant_loan_sql = "select i.name as industry, state_name, business_type, title, description, url, "
+      grant_loan_sql += "loan_type, state_name, agency, gov_type, is_general_purpose, is_development, "
+      grant_loan_sql += "is_exporting, is_contractor, is_green, is_military, is_minority, is_woman, "
+      grant_loan_sql += "is_disabled, is_rural, is_disaster "
+      grant_loan_sql += "from grant_loans gl "
+      grant_loan_sql += "left join grant_loans_industry gli on gl.id = gli.grant_loans_id "
+      grant_loan_sql += "left join industries i on i.id = gli.industry_id "
+      grant_loan_sql += "where i.name = ?"
+      GrantLoan.find_by_sql([grant_loan_sql,industry])
     end
     
     def GrantLoanHelper.getStateIDFromStateAlpha(state_alpha)
